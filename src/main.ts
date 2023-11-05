02d50162bbe2da79eeb6323f63ecb4d956e316d0
Wael Rassaa <wael@it-labs.fr>
2023-11-05 20:19:26 +0100

    fix-import-issue

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+export * from './numeric';
+export * from './string';
+export * from './array';
+export * from './json';
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { lib: { entry: resolve(__dirname, 'src/main.ts'), formats: ['es'] } },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'magneto-utils',
+      formats: ['es', 'umd'],
+    },
+  },
   resolve: { alias: { src: resolve('src/') } },
 });
